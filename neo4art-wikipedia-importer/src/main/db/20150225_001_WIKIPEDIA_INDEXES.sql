6d03972560ae7a0075a3dc227e8e9f5f424f48d1
Lorenzo Speranzoni <lorenzo.speranzoni@larus-ba.it>
2015-03-29 16:58:11 +0200

    new wikipedia import version based on GraphDatabaseService

--- neo4art-wikipedia-importer/src/main/db/20150225_001_WIKIPEDIA_INDEXES.sql
+++
@@ -1,4 +0,0 @@
-create index on :Wikipedia(title);
-create index on :WikipediaPage(title);
-create index on :WikipediaCategory(title);
-create index on :WikipediaGeneric(title);
--- neo4art-wikipedia-importer/src/main/java/org/neo4art/importer/wikipedia/domain/WikipediaInfoboxParser.java
+++
@@ -1,7 +0,0 @@
-package org.neo4art.importer.wikipedia.domain;
-
-public interface WikipediaInfoboxParser {
-
-	WikipediaInfobox parseInfobox(String infobox);
-	
-}
