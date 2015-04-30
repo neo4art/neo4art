b192b14de18b91d2c568ac41433a62bc715eb4b3
Gianmarco <Gianmarco@Gianmarco-Larus>
2015-04-30 13:19:13 +0200

    first commit neo4art Frontend

---
+++ neo4art-webapp/src/main/webapp/resources/js/colorchart.js
@@ -0,0 +1,288 @@
+// ################################## TIMELINE ##############################à
+var timeline;
+var bigData;
+
+$(document).ready(function(d) {
+	p = parseURLParams();
+	if (p != undefined) {
+		load(p);
+	} else {
+		console.log("non faccio niente");
+	}
+});
+function wantsGraph() {
+	p = parseURLParams();
+	if (p != undefined) {
+		window.location.href = "graph.html?query=" + p.query;
+	} else {
+		window.location.href = "graph.html";
+	}
+}
+function parseURLParams() {
+	var url = window.location.search;
+	var queryStart = url.indexOf("?") + 1, queryEnd = url.indexOf("#") + 1 || url.length + 1, query = url.slice(queryStart, queryEnd - 1), pairs = query
+	/* .replace(/\+/g, " ") */.split("&"), parms = {}, i, n, v, nv;
+
+	if (query === url || query === "") {
+		return;
+	}
+
+	for (i = 0; i < pairs.length; i++) {
+		nv = pairs[i].split("=");
+		n = decodeURIComponent(nv[0]);
+		v = decodeURIComponent(nv[1]);
+
+		if (!parms.hasOwnProperty(n)) {
+			parms[n] = [];
+		}
+
+		parms[n].push(nv.length === 2 ? v : null);
+	}
+	return parms;
+}
+
+function load(p) {
+	var parseDate = d3.time.format("%d-%b-%Y %H:%M").parse;
+	var serviceUrl = "http://localhost:8080/neo4art-services/api/services/timeline/search.json?searchInput="
+			+ p.query.toString().replace(/\+/g, " ");
+	$.ajax({
+		method : 'get',
+		url : "http://localhost:8080/neo4art-services/api/services/timeline/search.json?searchInput="
+				+ p.query.toString().replace(/\+/g, " "),
+		dataType : 'json',
+		success : function(graph) {
+			// specify options
+			var minData = new Date(graph[0].start);
+			minData.setMonth(-1);
+			var maxData = new Date(graph[graph.length - 1].start);
+			maxData.setMonth(13);
+			var options = {
+				width : "100%",
+				height : "300px",
+				editable : false,
+				"min" : minData, // lower limit of visible range
+				"max" : maxData,
+				layout : "box"
+			};
+			// Instantiate our timeline object.
+			timeline = new links.Timeline(document.getElementById('mytimeline'), options);
+			// }
+			// }
+			// d3.json("dati.json", function(error, graph) {
+			$.each(graph, function() {
+				this.start = parseDate(this.start);
+				if (this.end != undefined) {
+					this.end = parseDate(this.end);
+				}
+				if (this.thumbnail != undefined && this.description != undefined) {
+					this.content = "<img src='"+this.thumbnail+"' height='50px' width='50px' title='"+this.description+"'/>";
+				}
+			});
+			links.events.addListener(timeline, 'rangechange', onRangeChange);
+			links.events.addListener(timeline, 'rangechanged', onRangeChanged);
+			// Draw our timeline with the created data and options
+			bigData = graph;
+			timeline.draw(graph);
+			drawColorChart(graph, true);
+		},
+		error : function(error) {
+			console.log(error.status);
+		}
+	});
+}
+var actualLeft = null;
+function onRangeChange() {
+	var range = timeline.getVisibleChartRange();
+	if (actualLeft == null) {
+		actualLeft = $(".color-chart").position().left;
+	}
+	var left = timeline.dom.items.frame.style.left;
+	$(".color-chart").css("left", (actualLeft + parseFloat(left.substring(0, left.length - 2))));
+	$(".emotion-chart").css("left", (actualLeft + parseFloat(left.substring(0, left.length - 2))));
+	// vis2.setVisibleChartRange(range.start, range.end);
+	// document.getElementById('info').innerHTML += 'rangechanged ' +
+	// properties.start + ' - ' + properties.end + '<br>';
+}
+function onRangeChanged() {
+	// $(".color-chart").position().left;
+	actualLeft = null;
+	drawColorChart(bigData);
+}
+
+/**
+ * Function used by the two zoom buttons.
+ * 
+ * @param zoomVal
+ *            The value of zoom to be added (both positive or negative).
+ */
+function zoom(zoomVal) {
+	timeline.zoom(zoomVal);
+	timeline.trigger("rangechange");
+	timeline.trigger("rangechanged");
+}
+
+/**
+ * Adjust the visible time range such that all events are visible.
+ */
+function fitAllEventsZoom() {
+	timeline.setVisibleChartRangeAuto();
+}
+
+/**
+ * Function used by the two move buttons to navigate into the timeline
+ * 
+ * @param moveVal
+ *            The value (positive or negative) that is added to the current
+ *            position
+ */
+function move(moveVal) {
+	timeline.move(moveVal);
+	timeline.trigger("rangechange");
+	timeline.trigger("rangechanged");
+}
+
+/**
+ * Move the visible range such that the current time is located in the center of
+ * the timeline.
+ */
+function moveToCurrentTime() {
+	timeline.setVisibleChartRangeNow();
+}
+// ############################################# END TIMELINE
+
+function drawColorChart(data, parseColors) {
+	d3.select(".color-chart").selectAll("*").remove();
+	d3.select(".emotion-chart").selectAll("*").remove();
+	var firstVisibleMilliseconds = timeline.getVisibleChartRange().start.valueOf();
+	var firstDataMilliseconds = timeline.getOptions().min.valueOf();
+	var visibleRangeMilliseconds = timeline.getVisibleChartRange().end.valueOf() - timeline.getVisibleChartRange().start.valueOf();
+	var containerWidth = d3.select(".color-chart-container").style("width").substr(0,
+			d3.select(".color-chart-container").style("width").length - 2) - 1;
+	var containerHeight = d3.select(".color-chart-container").style("height").substr(0,
+			d3.select(".color-chart-container").style("height").length - 2) - 1;
+	var totalRangeMilliseconds = timeline.getOptions().max.valueOf() - timeline.getOptions().min.valueOf();
+	var chartWidth = (totalRangeMilliseconds * containerWidth) / visibleRangeMilliseconds;
+
+	var offsetX = ((firstVisibleMilliseconds - firstDataMilliseconds) * chartWidth / totalRangeMilliseconds);
+	$(".color-chart").css("left", 2 - offsetX);
+	$(".emotion-chart").css("left", 2 - offsetX);
+
+	var margin = {
+		top : 20,
+		right : 0,
+		bottom : 30,
+		left : 0
+	}, width = chartWidth - margin.left - margin.right, height = containerHeight - margin.top - margin.bottom;
+
+	var parseDate = d3.time.format("%d-%b-%Y %H:%M").parse;
+
+	var x = d3.time.scale().range([ 0, width ]);
+
+	var y = d3.scale.linear().range([ height, 0 ]);
+
+	var xAxis = d3.svg.axis().scale(x).orient("bottom");
+
+	var yAxis = d3.svg.axis().scale(y).orient("left");
+
+	var line = d3.svg.line().x(function(d) {
+		return x(d.start);
+	}).y(function(d) {
+		return y(d.averageRgb);
+	});
+	function xx(e) {
+		return x(e.start);
+	}
+	function xxt(e) {
+		return "translate(" + x(e.start) + ",0)";
+	}
+	function yy(e) {
+		return y(e.averageRgb);
+	}
+	function emotion(e) {
+		var txt;
+		if (e.emotion == "frown") {
+			txt = "\uf119";
+		}
+		if (e.emotion == "meh") {
+			txt = "\uf11A";
+		}
+		if (e.emotion == "smile") {
+			txt = "\uf118";
+		}
+		return txt;
+	}
+	function showData(obj, d) {
+		var coord = d3.mouse(obj);
+		var infobox = d3.select(".infobox");
+		// now we just position the infobox roughly where our mouse is
+		infobox.style("left", (coord[0] - 40) + "px");
+		infobox.style("top", (coord[1] - 220) + "px");
+		$(".infobox").html("&nbsp;&nbsp;");
+		$(".infobox").show();
+		$(".infobox").css("background-color", "#" + d.toString(16));
+	}
+	function hideData() {
+		$(".infobox").hide();
+	}
+	var svg = d3.select(".color-chart").append("svg").attr("width", width + margin.left + margin.right).attr("height",
+			height + margin.top + margin.bottom).append("g").attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+	var emo = d3.select(".emotion-chart").append("svg").attr("width", width + margin.left + margin.right).attr("height",
+			height + margin.top + margin.bottom).append("g").attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+	// d3.json("dati.json", function(error, data) {
+	var monthNames = [ "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec", "Jan" ];
+	var dateMin = timeline.getOptions().min;
+	var dateMax = timeline.getOptions().max;
+	var minMin = (dateMin.getMinutes() < 10) ? ("0" + dateMin.getMinutes()) : dateMin.getMinutes();
+	var minMax = (dateMax.getMinutes() < 10) ? ("0" + dateMax.getMinutes()) : dateMax.getMinutes();
+
+	if (parseColors) {
+		data.push({
+			start : dateMax.getDate() + "-" + monthNames[dateMax.getMonth() - 1] + "-" + dateMax.getFullYear() + " " + dateMax.getHours()
+					+ ":" + minMax,
+			content : "",
+			averageRgb : "0"
+		});
+		data.splice(0, 0, {
+			start : dateMin.getDate() + "-" + monthNames[dateMin.getMonth() - 1] + "-" + dateMin.getFullYear() + " " + dateMin.getHours()
+					+ ":" + minMin,
+			content : "",
+			averageRgb : "FFFFFF"
+		});
+	}
+	$.each(data, function(i, d) {
+		if (typeof d.start == "string") {
+			d.start = parseDate(d.start);
+		}
+		if (parseColors) {
+			d.averageRgb = parseInt(d.averageRgb, 16);
+		}
+	});
+
+	x.domain(d3.extent(data, function(d) {
+		return d.start;
+	}));
+	y.domain(d3.extent(data, function(d) {
+		return d.averageRgb;
+	}));
+	var emog = emo.selectAll("g").data(data).enter().append("g").attr("transform", xxt);
+	emog.append("circle").attr("fill", "#FFFFFF").attr("r", 15);// .attr("cx",
+	// xx);
+	emog.append("text").text(emotion).attr("class", "icon-text").attr("x", -13).attr("y", 11).attr("fill", "#444444");
+	svg.append("g").attr("class", "x axis").attr("transform", "translate(0," + height + ")").call(xAxis);
+	// svg.append("g").attr("class", "y
+	// axis").call(yAxis).append("text").attr("transform",
+	// "rotate(-90)").attr("y", 6)
+	// .attr("dy", ".71em").style("text-anchor", "end").text("Price ($)");
+	svg.append("path").datum(data).attr("class", "line").attr("d", line);
+	svg.selectAll("circle").data(data).enter().append("circle").attr("fill", function(d) {
+		return "#" + d.averageRgb.toString(16);
+	}).attr("r", 10).attr("cx", xx).attr("cy", yy).on("mouseover", function(d) {
+		showData(this, d.averageRgb);
+	}).on("mouseout", function() {
+		hideData();
+	}).attr("class", "pallino");
+	$(".color-chart").append("<div class='infobox' style='display:none;'>Test</div>");
+
+	// });
+}
---
+++ neo4art-webapp/src/main/webapp/resources/js/graph.js
@@ -0,0 +1,444 @@
+/** Base radius of each node */
+var radius = 25;
+/** When a node is clicked I keep a reference to it */
+var selectedNode;
+/** I keep a global reference of the zoom */
+var currentZoom = 1;
+/** Global reference to the actual translation */
+var currentTranslateX = 0;
+/** Global reference to the actual translation */
+var currentTranslateY = 0;
+
+/**
+ * I call this after the document is ready because I need to know at least the
+ * dimensions of the document
+ */
+$(document).ready(function() {
+	// $.ajax({
+	// dataType: "json",
+	// url: url,
+	// data: data,
+	// success: success
+	// });
+	// console.log(parseURLParams().query);
+	p = parseURLParams();
+	if (p != undefined) {
+		graph = new theGraph();
+	} else {
+		console.log("non faccio niente");
+	}
+});
+
+function wantsTimeline() {
+	p = parseURLParams();
+	if (p != undefined) {
+		window.location.href = "timeline.html?query=" + p.query;
+	} else {
+		window.location.href = "timeline.html";
+	}
+}
+
+function parseURLParams() {
+	var url = window.location.search;
+	var queryStart = url.indexOf("?") + 1, queryEnd = url.indexOf("#") + 1 || url.length + 1, query = url.slice(queryStart, queryEnd - 1), pairs = query
+	/* .replace(/\+/g, " ") */.split("&"), parms = {}, i, n, v, nv;
+
+	if (query === url || query === "") {
+		return;
+	}
+
+	for (i = 0; i < pairs.length; i++) {
+		nv = pairs[i].split("=");
+		n = decodeURIComponent(nv[0]);
+		v = decodeURIComponent(nv[1]);
+
+		if (!parms.hasOwnProperty(n)) {
+			parms[n] = [];
+		}
+
+		parms[n].push(nv.length === 2 ? v : null);
+	}
+	return parms;
+}
+
+function legend() {
+	alert("")
+}
+
+function theGraph() {
+	var selectedNode, selectedD;
+	generateWindow();
+	firstUpdate();
+
+	addNode = function(nodeToAdd) {
+		nodeList.push({
+			"id" : nodeToAdd.id,
+			"name" : nodeToAdd.name,
+			"group" : nodeToAdd.group,
+			"thumbnail" : nodeToAdd.thumbnail,
+			"type":nodeToAdd.type,
+			"link":nodeToAdd.link,
+			"description":nodeToAdd.description
+		});
+	}
+	addLink = function(linkToAdd) {
+		var sourceNode = findNode(linkToAdd.source);
+		var targetNode = findNode(linkToAdd.target);
+
+		if ((sourceNode !== undefined) && (targetNode !== undefined)) {
+			linkList.push({
+				"source" : linkToAdd.source,
+				"target" : linkToAdd.target,
+				"value" : linkToAdd.value,
+				"linkName" : linkToAdd.linkName
+			});
+			update();
+		}
+	}
+
+	this.removeNode = function(id) {
+		var i = 0;
+		var n = findNode(id);
+		while (i < linkList.length) {
+			if ((linkList[i]['source'] === n) || (linkList[i]['target'] == n))
+				linkList.splice(i, 1);
+			else
+				i++;
+		}
+		var index = findNodeIndex(id);
+		if (index !== undefined) {
+			nodeList.splice(index, 1);
+			update();
+		}
+	}
+
+	var findNode = function(id) {
+		for (var i = 0; i < nodeList.length; i++) {
+			if (nodeList[i].id === id)
+				return nodeList[i]
+		}
+	}
+
+	var findNodeIndex = function(id) {
+		for (var i = 0; i < nodeList.length; i++) {
+			if (nodeList[i].id === id)
+				return i
+		}
+	}
+	clearDiv("graph");
+	var width = $("#graph").width();
+	var height = $("#graph").height();
+
+	var color = d3.scale.category20();
+
+	var force = d3.layout.force().charge(-300).linkDistance(150).size([ width, height ]);
+
+	var zoom = d3.behavior.zoom().on("zoom", redraw);
+
+	var drag = force.drag().on("dragstart", dragstarted);
+
+	var svg = d3.select("#graph").append("svg").attr("class", "svg").append("g").call(zoom).on("dblclick.zoom", null);
+
+	var rect = svg.append("rect").attr("width", "100%").attr("height", "100%").style("fill", "none").style("pointer-events", "all");
+
+	svg.append("text").attr("x", 10).attr("y", 20).attr("font-family", "sans-serif").attr("font-size", "15px").attr("fill", "#333333")
+			.text("Right Click to show details");
+
+	/* Contenitore del grafo */
+	var container = svg.append("g").attr("id", "container");
+
+	var nodeList = [];
+	var linkList = [];
+
+	var nodeEnter, linkEnter;
+	function firstUpdate(json) {
+		var serviceUrl = "http://localhost:8080/neo4art-services/api/services/search/search-results.json?searchInput="
+				+ p.query.toString().replace(/\+/g, " ");
+		$.ajax({
+			method : 'get',
+			url : "http://localhost:8080/neo4art-services/api/services/search/search-results.json?searchInput="
+					+ p.query.toString().replace(/\+/g, " "),
+			dataType : 'json',
+			success : function(graph) {
+				// d3.json("miserables.json", function(error, graph) {
+				force.nodes(graph.nodeList).links(graph.linkList).start();
+				nodeList = force.nodes();
+				linkList = force.links();
+				update();
+			}
+		});
+	}
+	function update() {
+		clearDiv("container");
+
+		function connects(l, n) {
+			if (l.source.id == n.id || l.target.id == n.id) {
+				return true;
+			}
+			return false;
+		}
+
+		var link = container.append("g").attr("class", "links").selectAll(".link").data(linkList, function(d) {
+			return d.source.name + "-" + d.target.name;
+		});
+		linkEnter = link.enter().append("line").attr("class", "link").style("stroke-width", function(d) {
+			return Math.sqrt(d.value) * 3;
+		}).on("mouseover", function(d) {
+			d3.select(this).classed("link-active-green", true);
+			node.classed("node-active-green", function(o) {
+				thisOpacity = connects(d, o) ? true : false;
+				return thisOpacity;
+			});
+		}).on("mouseout", function(d) {
+			node.classed("node-active-green", false);
+			link.classed("link-active-green", false);
+		});
+		linkEnter.append("title").text(function(d) {
+			return d.linkName
+		});
+		link.exit().remove();
+
+		// ########################################
+		var nodeContainer = container.append("g").attr("class", "nodes");
+		var node = nodeContainer.selectAll(".node").data(nodeList, function(d) {
+			return d.name;
+		});
+		nodeEnter = node.enter().append("g").attr("class", "node").attr("id", function(d, i) {
+			// d.id = i;
+			d.radius = radius;
+			return d.id; // TODO CHECK
+		})/*
+			 * .attr("transform", function(d) { d.radius = radius;// d.weight +
+			 * 12; // RAGGIO return "translate(" + d.x + "," + d.y + ")"; })
+			 */.call(drag);
+
+		nodeEnter.append("circle").attr("r", function(d) {
+			return d.radius;// + 2;
+		}).style("fill", function(d) {
+			return color(1 / d.rating);
+		});
+
+		nodeEnter.append("clipPath").attr('id', function(d, i) {
+			return "clip" + i
+		}).append("circle").attr("class", "clip-path").attr("r", function(d) {
+			return d.radius;
+		}).style("fill", function(d) {
+			return color(1 / d.rating);
+		});
+
+		var img = nodeEnter.append("svg:image").attr("class", "circle").attr("xlink:href", function(d) {
+			return d.thumbnail;
+		}).attr("clip-path", function(d, i) {
+			return "url(#clip" + i + ")"
+		}).attr("x", function(d) {
+			return -d.radius;
+		}).attr("y", function(d) {
+			return -d.radius;
+		}).attr("width", function(d) {
+			return d.radius * 2;
+		}).attr("height", function(d) {
+			return d.radius * 2;
+		}).attr("viewBox",function(d){
+			return (-d.radius) +" "+(-d.radius) +" "+(d.radius*2) + " "+ (d.radius*2);
+		});
+
+		nodeEnter.append("title").text(function(d) {
+			return d.name;
+		});
+
+		var linkedByIndex = {};
+		linkList.forEach(function(d) {
+			linkedByIndex[d.source.index + "," + d.target.index] = 1;
+		});
+
+		function isConnected(a, b) {
+			return linkedByIndex[a.index + "," + b.index] || linkedByIndex[b.index + "," + a.index];
+		}
+
+		nodeEnter.on(
+				"mouseover",
+				function(d) {
+					if (!d.clicked) {
+						node.classed("node-active", function(o) {
+							thisOpacity = isConnected(d, o) ? true : false;
+							this.setAttribute('fill-opacity', thisOpacity);
+							return thisOpacity;
+						});
+
+						link.classed("link-active", function(o) {
+							return o.source === d || o.target === d ? true : false;
+						});
+
+						d3.select(this).classed("node-active", true);
+						d3.select(this).selectAll("circle").transition().duration(750).attr("r", function() {
+							if (d3.select(this).classed("clip-path"))
+								return d.radius * 1.5;
+							else
+								return (d.radius /* + 2 */) * 1.5
+						});
+						d3.select(this).select("image").transition().duration(750).attr("x", -d.radius * 1.5).attr("y", -d.radius * 1.5)
+								.attr("width", (d.radius * 2) * 1.5).attr("height", (d.radius * 2) * 1.5);
+					}
+				})
+		.on(
+				"mouseout",
+				function(d) {
+					if (!d.clicked) {
+						node.classed("node-active", false);
+						link.classed("link-active", false);
+
+						d3.select(this).selectAll("circle").transition().duration(750).attr("r", function() {
+							if (d3.select(this).classed("clip-path"))
+								return d.radius;
+							else
+								return d.radius;// + 2
+						});
+						d3.select(this).select("image").transition().duration(750).attr("x", -d.radius).attr("y", -d.radius).attr("width",
+								d.radius * 2).attr("height", d.radius * 2);
+					}
+				});
+
+		nodeEnter
+				.on(
+						"contextmenu",
+						function(d) {
+							// PUTS THE NODE IN ORDER SO AS THE
+							// CLICKED IS ON TOP
+							nodeContainer.selectAll("g").sort(function(a, b) {
+								var i = (a.id != d.id) ? -1 : 1;
+								return i;
+							});
+							openWindow(d);
+							selectedNode = d3.select(this);
+							selectedD = d;
+							d.clicked = true;
+							node.classed("node-clicked", false);
+							link.classed("link-clicked", false);
+							d3.select(this).selectAll("circle").transition().duration(750).attr("r", function() {
+								if (d3.select(this).classed("clip-path"))
+									return d.radius * 5 / currentZoom;
+								else
+									return d.radius * 5 / currentZoom;// + 2
+							});
+							d3.select(this).select("image").transition().duration(750).attr("x", -d.radius * 5 / currentZoom).attr("y",
+									-d.radius * 5 / currentZoom).attr("width", d.radius * 2 * 5 / currentZoom).attr("height",
+									d.radius * 2 * 5 / currentZoom);
+							console.log("w:" + width + " d.x:" + d.x + " ctx:" + currentTranslateX + " calc:"
+									+ ((width / 4 / currentZoom) - d.x + (currentTranslateX / currentZoom)) + " z:" + currentZoom);
+							container
+									.transition()
+									.duration(750)
+									.attr(
+											"transform",
+											"translate("
+													+ (currentTranslateX + ((((width / 4) / currentZoom) - (d.x + (currentTranslateX / currentZoom))) * currentZoom))
+													+ ","
+													+ (currentTranslateY + ((((height / 2) / currentZoom) - (d.y + (currentTranslateY / currentZoom))) * currentZoom))
+													+ ")" + " scale(" + currentZoom + ")");
+							d3.event.preventDefault();
+						});
+		nodeEnter.on("dblclick", function(d) {
+			$.ajax({
+				method : 'get',
+				url : "http://localhost:8080/neo4art-services/api/services/search/node-explode.json?nodeId="
+						+ d.id,
+				dataType : 'json',
+				success : function(graphExp) {
+					// d3.json("explode.json", function(error, graphExp) {
+					$.each(graphExp.nodeList, function(i) {
+						addNode(this);
+					});
+					update();
+					$.each(graphExp.linkList, function(i) {
+						addLink(this);
+					});
+				}
+			});
+		});
+
+		node.exit().remove();
+		// ##########################################
+
+		force.on("tick", function() {
+			link.attr("x1", function(d) {
+				return d.source.x;
+			}).attr("y1", function(d) {
+				return d.source.y;
+			}).attr("x2", function(d) {
+				return d.target.x;
+			}).attr("y2", function(d) {
+				return d.target.y;
+			});
+
+			node.attr("transform", function(d) {
+				return "translate(" + d.x + "," + d.y + ")";
+			});
+		});
+
+		// Restart the force layout.
+		force.start();
+	}
+	function redraw() {
+		currentZoom = d3.event.scale;
+		currentTranslateX = d3.event.translate[0];
+		currentTranslateY = d3.event.translate[1];
+		container.attr("transform", "translate(" + d3.event.translate + ")" + " scale(" + d3.event.scale + ")");
+		force.resume();
+	}
+
+	function dragstarted(d) {
+		d3.event.sourceEvent.stopPropagation();
+		d3.select(this).classed("dragging", true);
+	}
+
+	function generateWindow() {
+		clearDiv("floating");
+		var float = d3.select("#floating");
+		$("#floating").resizable({
+			handles : "w"
+		});
+		var bttX = float.append("div").attr("class", "ics").text("X");
+		bttX.on("click", closeWindow);
+		float.append("div").attr("id", "data");
+	}
+
+	function openWindow(d) {
+		clearDiv("data");
+		var float = d3.select("#floating").attr("class", "visible").attr("style", null);
+		var data = float.select("#data");
+		data.append("div").attr("class", "title").append("h1").text(d.name);
+		data.append("div").attr("class", "thumbnail").append("img").attr("src", d.thumbnail);
+		data.append("div").attr("class", "description").text(d.description);
+		data.append("div").attr("class", "link").html("</br><a href='"+d.link+"'>"+d.link+"</a>");
+		$("#floating").perfectScrollbar();
+		$("#floating").perfectScrollbar('update');
+	}
+
+	function closeWindow() {
+		d3.select("#floating").attr("class", null).style("left", null);
+		container.transition().duration(750).attr("transform",
+				"translate(" + currentTranslateX + "," + currentTranslateY + ")" + " scale(" + currentZoom + ")");
+		nodeEnter.classed("node-active", false);
+		linkEnter.classed("link-active", false);
+
+		container.selectAll("circle").transition().duration(750).attr("r", function() {
+			if (container.classed("clip-path"))
+				return radius;
+			else
+				return radius;// + 2
+		});
+		selectedD.clicked = false;
+		selectedNode.select("image").transition().duration(750).attr("x", -radius).attr("y", -radius).attr("width", radius * 2).attr(
+				"height", radius * 2);
+	}
+
+}
+/**
+ * This method remove all other components inside the specified ID
+ * 
+ * @param elementID
+ *            The id of the div
+ */
+function clearDiv(elementID) {
+	document.getElementById(elementID).innerHTML = "";
+}
