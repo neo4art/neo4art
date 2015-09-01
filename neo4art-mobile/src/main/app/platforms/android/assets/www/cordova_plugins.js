28fd4e01944ce04ff8b559a9bda8aea510d1fa5a
nico-Fritz <nico_fritz@hotmail.it>
2015-09-01 17:54:45 +0200

    Added neo4art-mobile basic project

---
+++ neo4art-mobile/src/main/app/platforms/android/assets/www/cordova_plugins.js
@@ -0,0 +1,33 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "file": "plugins/cordova-plugin-whitelist/whitelist.js",
+        "id": "cordova-plugin-whitelist.whitelist",
+        "runs": true
+    },
+    {
+        "file": "plugins/plugin.http.request/www/http-request.js",
+        "id": "plugin.http.request.phonegap-http-requst",
+        "clobbers": [
+            "cordova.plugins.http-request"
+        ]
+    },
+    {
+        "file": "plugins/plugin.google.maps/www/googlemaps-cdv-plugin.js",
+        "id": "plugin.google.maps.phonegap-googlemaps-plugin",
+        "clobbers": [
+            "plugin.google.maps"
+        ]
+    }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+    "cordova-plugin-whitelist": "1.0.0",
+    "org.apache.cordova.geolocation": "0.3.12",
+    "plugin.http.request": "1.0.4",
+    "plugin.google.maps": "1.2.7",
+    "com.google.playservices": "23.0.0"
+}
+// BOTTOM OF METADATA
+});
